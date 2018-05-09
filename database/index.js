8be1b9252c19b1f960aefecd86a10b3b34803f24
Century <favouronu@gmail.com>
2018-05-09 23:43:55 +0000

    small fix

--- database/index.js
+++ database/index.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "project"
+    host: "mysql7002.site4now.net",
+    user: "lqpgny8x_ceghana",
+    password: "savour2how.roll",
+    database: "lqpgny8x_project"
 });
 
 conn.connect(err => {
